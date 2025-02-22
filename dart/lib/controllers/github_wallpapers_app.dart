cd572e31a35e27f2e081a0284c3acdddf75e5835
Luca Raffo <55745404+Lucaffo@users.noreply.github.com>
2025-02-22 15:53:10 +0100

    github wallpaper app controller, close #13

---
+++ dart/lib/controllers/github_wallpapers_app.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'dart:html';
+import 'dart:math';
+import 'package:dart/views/wallpaper_canvas.dart';
+import 'package:dart/views/wallpaper_code_editor.dart';
+import 'package:wallpaper/wallpaper.dart';
+
+/**
+ * The Github Wallpaper App
+ * This is the main controller of the application lifecycle.
+ * 
+ * 22/02/2025 @ Luca Raffo
+ */
+class GithubWallpapersApp {
+
+  static const int numberOfConfigurations = 7;
+  Timer? _debounceTimer;
+  
+  final ButtonElement saveBtn = document.querySelector("#save-btn") as ButtonElement;
+  final WallpaperCanvas canvas = WallpaperCanvas("output");
+  final WallpaperCodeEditor editor = WallpaperCodeEditor("input");
+
+  Future setFirstWallpaper() async {
+    HTTPWallpaperFactory httpWallpaperFactory = HTTPWallpaperFactory();
+    Wallpaper? wallpaper = await httpWallpaperFactory.getWallpaper(getRandomInitialConfiguration());
+    setWallpaper(wallpaper);
+  }
+
+  Uri getRandomInitialConfiguration() {
+    int index = Random.secure().nextInt(numberOfConfigurations) + 1;
+    return Uri.parse("https://lucaffo.github.io/github-wallpapers/static/wallpapers/wallpaper_${index.toString().padLeft(2, '0')}.json");
+  }
+
+  void bindUI() {
+    saveBtn.onClick.listen((_) => canvas.saveImage());
+    editor.onChange(debounceRefreshWallpaper);
+  }
+
+  void debounceRefreshWallpaper() {
+    _debounceTimer?.cancel();
+    _debounceTimer = Timer(Duration(milliseconds: 1000), () => refreshWallpaper());
+  }
+
+  void refreshWallpaper() {
+    print("Wallpaper Refresh");
+    Wallpaper? wallpaper = editor.getWallpaper();
+    if(wallpaper == null) return;
+    canvas.setWallpaper(wallpaper);
+  }
+
+  void setWallpaper(Wallpaper? wallpaper) {
+    if (wallpaper == null) return;
+    editor.setWallpaper(wallpaper);
+    canvas.setWallpaper(wallpaper);
+  }
+}
--- dart/web/main.dart
+++ dart/web/main.dart
@@ -1,57 +1,8 @@
-import 'dart:math';
-import 'dart:html';
-import 'dart:async';
+import 'package:dart/controllers/github_wallpapers_app.dart';
 
-import 'package:dart/views/wallpaper_canvas.dart';
-import 'package:dart/views/wallpaper_code_editor.dart';
-import 'package:wallpaper/wallpaper.dart';
-
-final ButtonElement saveBtn = document.querySelector("#save-btn") as ButtonElement;
-
-const int numberOfConfigurations = 7;
-Timer? _debounceTimer;
-
-final WallpaperCanvas canvas = WallpaperCanvas("output");
-final WallpaperCodeEditor editor = WallpaperCodeEditor("input");
+final GithubWallpapersApp app = new GithubWallpapersApp();
 
 void main() async {
-  // Set first wallpaper
-  await setFirstWallpaper();
-
-  // Bind the UI
-  bindUI();
-}
-
-Future setFirstWallpaper() async {
-  HTTPWallpaperFactory httpWallpaperFactory = HTTPWallpaperFactory();
-  Wallpaper? wallpaper = await httpWallpaperFactory.getWallpaper(getRandomInitialConfiguration());
-  setWallpaper(wallpaper);
-}
-
-Uri getRandomInitialConfiguration() {
-  int index = Random.secure().nextInt(numberOfConfigurations) + 1;
-  return Uri.parse("https://lucaffo.github.io/github-wallpapers/static/wallpapers/wallpaper_${index.toString().padLeft(2, '0')}.json");
-}
-
-void bindUI() {
-  saveBtn.onClick.listen((_) => canvas.saveImage());
-  editor.onChange(debounceRefreshWallpaper);
-}
-
-void debounceRefreshWallpaper() {
-  _debounceTimer?.cancel();
-  _debounceTimer = Timer(Duration(milliseconds: 1000), () => refreshWallpaper());
-}
-
-void refreshWallpaper() {
-  print("Wallpaper Refresh");
-  Wallpaper? wallpaper = editor.getWallpaper();
-  if(wallpaper == null) return;
-  canvas.setWallpaper(wallpaper);
-}
-
-void setWallpaper(Wallpaper? wallpaper) {
-  if (wallpaper == null) return;
-  editor.setWallpaper(wallpaper);
-  canvas.setWallpaper(wallpaper);
+  await app.setFirstWallpaper();
+  app.bindUI();
 }
